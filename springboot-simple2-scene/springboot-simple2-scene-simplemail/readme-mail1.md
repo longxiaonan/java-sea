23e28984b042f521c9c5b9ce79cfba4335649665
longxiaonan@163.com <longxiaonan@163.com>
2019-06-10 23:44:02 +0800

    集成邮件发送功能

---
+++ springboot-simple2-scene/springboot-simple2-scene-simplemail/readme-mail1.md
@@ -0,0 +1,7 @@
+测试地址：
+简单普通邮件：
+http://localhost:8080/mail/send1
+静态html邮件：
+http://localhost:8080/mail/send2
+freemarker邮件：
+http://localhost:8080/mail/send3
---
+++ springboot-simple2-scene/springboot-simple2-scene-simplemail/src/main/java/com/lance/mail/springbootmail/controller/MailController.java
@@ -0,0 +1,50 @@
+package com.lance.mail.springbootmail.controller;
+
+import com.lance.mail.springbootmail.bean.MailBean;
+import com.lance.mail.springbootmail.util.MailUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MailController
+ * @Description TODO
+ * @Author longxiaonan@163.com
+ * @Date 2019/6/10 0010 23:10
+ */
+@RequestMapping("/mail")
+@RestController
+public class MailController {
+
+    @Autowired
+    MailUtil mailUtil;
+
+    @GetMapping("/send1")
+    public void test(){
+
+        MailBean record = new MailBean();
+        record.setSubject("测试邮件");
+        record.setRecipient("longxiaonan@163.com");
+        record.setContent("测试邮件内容");
+        mailUtil.sendSimpleMail(record);
+    }
+    @GetMapping("/send2")
+    public void test2(){
+
+        MailBean record = new MailBean();
+        record.setSubject("测试邮件");
+        record.setRecipient("longxiaonan@163.com");
+        record.setContent("测试邮件内容");
+        mailUtil.sendHTMLMail(record);
+    }
+    @GetMapping("/send3")
+    public void test3(){
+
+        MailBean record = new MailBean();
+        record.setSubject("测试邮件");
+        record.setRecipient("longxiaonan@163.com");
+        record.setContent("测试邮件内容");
+        mailUtil.sendTemplateMail(record);
+    }
+}
