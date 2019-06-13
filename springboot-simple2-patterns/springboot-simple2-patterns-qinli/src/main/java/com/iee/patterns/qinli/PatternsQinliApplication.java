c469b177be2b94fe7fef7d9739c659426bb07f8b
longxiaonan@163.com <longxiaonan@163.com>
2019-06-13 11:01:35 +0800

    开始写patterns

---
+++ springboot-simple2-patterns/springboot-simple2-patterns-qinli/src/main/java/com/iee/patterns/qinli/PatternsQinliApplication.java
@@ -0,0 +1,12 @@
+package com.iee.patterns.qinli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsQinliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternsQinliApplication.class, args);
+	}
+}
