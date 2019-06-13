5fde2670be4869f6951de756bbcfa0ca08f0853b
longxiaonan@163.com <longxiaonan@163.com>
2019-06-13 11:09:59 +0800

    修改主类名称

--- springboot-simple2-patterns/springboot-simple2-patterns/src/main/java/com/iee/patterns/PatternsApplication.java
+++ springboot-simple2-patterns/springboot-simple2-patterns/src/main/java/com/iee/patterns/PatternsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatternsQinliApplication {
+public class PatternsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatternsQinliApplication.class, args);
+		SpringApplication.run(PatternsApplication.class, args);
 	}
 }
