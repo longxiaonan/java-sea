09813144027b1ee1593e01555ba06ad423d00ca8
longxiaonan@163.com <longxiaonan@163.com>
2019-06-13 20:54:25 +0800

    完成springboot上传整合

---
+++ springboot-simple2-file/springboot-simple2-file-upload/readme-file-upload.md
@@ -0,0 +1,12 @@
+# 文件上传整合
+## 使用到的框架
+thymeleaf：用于显示文件上传的页面，方便测试。当然，也可以用pastman等工具测试。
+
+## 整合步骤
+### 添加依赖
+### 编写controller
+## 测试
+上传页面URL：http://localhost:8080/upload
+
+
+
---
+++ springboot-simple2-file/springboot-simple2-file-upload/src/main/java/com/iee/file/upload/controller/FileUploadController.java
@@ -0,0 +1,56 @@
+package com.iee.file.upload.controller;
+
+import lombok.Cleanup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+
+@Controller
+public class FileUploadController {
+
+    @Value("${filePath}")
+    private String filePath;
+
+    @GetMapping("/upload")
+    public String uploading() {
+        //跳转到 templates 目录下的 uploading.html
+        return "upload";
+    }
+
+    //处理文件上传
+    @PostMapping("/uploading")
+    public @ResponseBody
+    String uploading(@RequestParam("file") MultipartFile file,
+                     HttpServletRequest request) {
+        try {
+            uploadFile(file.getBytes(), filePath, file.getOriginalFilename());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("文件上传失败!");
+            return "uploading failure";
+        }
+        System.out.println("文件上传成功!");
+        return "uploading success";
+    }
+
+
+
+    public void  uploadFile(byte[] file, String filePath, String fileName) throws Exception {
+        File targetFile = new File(filePath);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        @Cleanup FileOutputStream out = new FileOutputStream(filePath+fileName);
+        out.write(file);
+        out.flush();
+    }
+
+}
