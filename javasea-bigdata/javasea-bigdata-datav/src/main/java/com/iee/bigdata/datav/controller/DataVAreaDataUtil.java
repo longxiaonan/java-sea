483d3e15ff8e7aed40ca0b4ab7b011b1d7e492b2
longxiaonan@163.com <longxiaonan@163.com>
2019-10-17 20:27:07 +0800

    修改类名称

--- javasea-bigdata/javasea-bigdata-datav/src/main/java/com/iee/bigdata/datav/controller/DataVAreaDataUtil.java
+++ javasea-bigdata/javasea-bigdata-datav/src/main/java/com/iee/bigdata/datav/controller/DataVAreaDataUtil.java
@@ -10,12 +10,12 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * @ClassName DataVAreaData
+ * @ClassName DataVAreaDataUtil
  * @Description DataV的区域数据加载类, 用于大屏监控中进行查询的地理位置条件
  * @Author longxiaonan@163.com
  * @Date 2018/9/21 0021 16:58
  */
-public class DataVAreaData {
+public class DataVAreaDataUtil {
 
     //最外层的key用于索引, 可能是中国的区域id 100000, 或者是省份的区域id, 还可能是市区的区域id
     //里面的map是针对于第一个key的下级行政单位的区域id和行政单位名称.
@@ -23,14 +23,14 @@ public class DataVAreaData {
     //如: 最外层的key为440000, 那么下级的行政单位级别是市, 那么里层的map就是市的区域id和对应的市名称. 如{"440100":"广州市", "440600":"佛山市",...}.
     private static Map<String, Map<String, String>> areaMap = new HashMap<>();
 
-    private DataVAreaData() {
+    private DataVAreaDataUtil() {
     }
 
     private static class SingletonFactory {
-        private static DataVAreaData instance = new DataVAreaData();
+        private static DataVAreaDataUtil instance = new DataVAreaDataUtil();
     }
 
-    public static DataVAreaData getInstance() {
+    public static DataVAreaDataUtil getInstance() {
         //加载中国省份的区域code和省份名称.
         String result = HttpClientUtils.sendHttpGet("https://geo.datav.aliyun.com/areas/bound/100000_full.json");
         areaMap.put("100000", parseResult(result));
@@ -73,9 +73,9 @@ public class DataVAreaData {
     }
 
     public static void main(String[] args) {
-        System.out.println(DataVAreaData.getInstance().getAreaData());//默认是获取全国省(包括直辖市)的区域id和行政名
-        System.out.println(DataVAreaData.getInstance().getAreaData("110000"));//110000为北京市的区域id
-        System.out.println(DataVAreaData.getInstance().getAreaData("440000"));//440000为广东省的区域id
-        System.out.println(DataVAreaData.getInstance().getAreaData("441900"));//441900为东莞的区域id
+        System.out.println(DataVAreaDataUtil.getInstance().getAreaData());//默认是获取全国省(包括直辖市)的区域id和行政名
+        System.out.println(DataVAreaDataUtil.getInstance().getAreaData("110000"));//110000为北京市的区域id
+        System.out.println(DataVAreaDataUtil.getInstance().getAreaData("440000"));//440000为广东省的区域id
+        System.out.println(DataVAreaDataUtil.getInstance().getAreaData("441900"));//441900为东莞的区域id
     }
 }
