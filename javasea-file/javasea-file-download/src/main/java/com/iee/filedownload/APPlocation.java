2abd592fff965b601001358c1c6e3dd2f9280398
longxiaonan@163.com <longxiaonan@163.com>
2019-10-27 21:47:59 +0800

    调整部分依赖和文档描述

--- javasea-file/javasea-file-download/src/main/java/com/iee/filedownload/APPlocation.java
+++ javasea-file/javasea-file-download/src/main/java/com/iee/filedownload/APPlocation.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.iee.common","com.iee.filedownload"})
+@ComponentScan(basePackages = {"com.iee.mgr","com.iee.filedownload"})
 public class APPlocation {
     public static void main(String[] args) {
         SpringApplication.run(APPlocation.class, args);
