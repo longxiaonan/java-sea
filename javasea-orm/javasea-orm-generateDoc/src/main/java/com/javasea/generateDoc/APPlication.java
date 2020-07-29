8d65b217b98e623df3dc3702d0a54da126093c64
longxiaonan@163.com <longxiaonan@163.com>
2020-07-29 11:28:05 +0800

    新增项目：实现数据库文档生成

---
+++ javasea-orm/javasea-orm-generateDoc/src/main/java/com/javasea/generateDoc/APPlication.java
@@ -0,0 +1,16 @@
+package com.javasea.generateDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author longxiaonan@aliyun.com
+ */
+@SpringBootApplication
+public class APPlication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APPlication.class, args);
+    }
+
+}
---
+++ javasea-orm/javasea-orm-generateDoc/src/test/java/com/javasea/generateDoc/GenerateDoc.java
@@ -0,0 +1,81 @@
+package com.javasea.generateDoc;
+
+import cn.smallbun.screw.core.Configuration;
+import cn.smallbun.screw.core.engine.EngineConfig;
+import cn.smallbun.screw.core.engine.EngineFileType;
+import cn.smallbun.screw.core.engine.EngineTemplateType;
+import cn.smallbun.screw.core.execute.DocumentationExecute;
+import cn.smallbun.screw.core.process.ProcessConfig;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+
+/**
+ * @ClassName GenerateDoc
+ * @Description TODO
+ * @Author longxiaonan@163.com
+ * @Date 2020/7/28 0028 11:11
+ */
+public class GenerateDoc {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        //数据源
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikariConfig.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/lmwy2?useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("123456");
+        //设置可以获取tables remarks信息
+        hikariConfig.addDataSourceProperty("useInformationSchema", "true");
+        hikariConfig.setMinimumIdle(2);
+        hikariConfig.setMaximumPoolSize(5);
+        DataSource dataSource = new HikariDataSource(hikariConfig);
+        //生成配置
+        EngineConfig engineConfig = EngineConfig.builder()
+                //生成文件路径
+                .fileOutputDir("d:/")
+                //打开目录
+                .openOutputDir(true)
+                //文件类型
+                .fileType(EngineFileType.HTML)
+                //生成模板实现
+                .produceType(EngineTemplateType.freemarker).build();
+
+        //忽略表
+        ArrayList<String> ignoreTableName = new ArrayList<>();
+        ignoreTableName.add("test_user");
+        ignoreTableName.add("test_group");
+        //忽略表前缀
+        ArrayList<String> ignorePrefix = new ArrayList<>();
+        ignorePrefix.add("test_");
+        //忽略表后缀
+        ArrayList<String> ignoreSuffix = new ArrayList<>();
+        ignoreSuffix.add("_test");
+        ProcessConfig processConfig = ProcessConfig.builder()
+                //忽略表名
+                .ignoreTableName(ignoreTableName)
+                //忽略表前缀
+                .ignoreTablePrefix(ignorePrefix)
+                //忽略表后缀
+                .ignoreTableSuffix(ignoreSuffix).build();
+        //配置
+        Configuration config = Configuration.builder()
+                //版本
+                .version("1.0.0")
+                //描述
+                .description("数据库设计文档生成")
+                //数据源
+                .dataSource(dataSource)
+                //生成配置
+                .engineConfig(engineConfig)
+                //生成配置
+                .produceConfig(processConfig).build();
+        //执行生成
+        new DocumentationExecute(config).execute();
+    }
+
+}
